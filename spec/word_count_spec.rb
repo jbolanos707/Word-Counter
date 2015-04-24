47a739941cd769d471670b4d82db5c824b3060ff
Jennifer Bolanos <jbolanos707@gmail.com>
2015-04-24 11:57:48 -0700

    first spec passes: returns the word when the word is the same

---
+++ spec/word_count_spec.rb
@@ -0,0 +1,16 @@
+#Create a website that uses a method to return how frequently a word appears in a given string. The user should be able to input both the word and the string of words to check.
+
+#Example:
+
+#Word Input: happy
+
+#String Input: I am so very happy, happy, happy to be getting my code reviewed today.
+
+require('rspec')
+require('./lib/word_count')
+
+describe('String#word_count') do
+  it("returns the word if the word is the same") do
+   expect(('happy').word_count()).to(eq('happy'))
+  end
+end
